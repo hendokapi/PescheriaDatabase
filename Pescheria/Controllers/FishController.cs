d9cf4e0cbf11d1a08fc1c7ad1dd73232f8293bc0
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-02-29 15:59:15 +0400

    add delete and things

--- Pescheria/Controllers/FishController.cs
+++ Pescheria/Controllers/FishController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Pescheria.Models;
 using Microsoft.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace Pescheria.Controllers
@@ -64,8 +65,6 @@ namespace Pescheria.Controllers
             {
                 // aprire la connessione
                 var conn = new SqlConnection(connString);
-                
-
                 try
                 {
                     conn.Open();
@@ -111,25 +110,47 @@ namespace Pescheria.Controllers
         [HttpPost]
         public IActionResult Add(Fish fish, IFormFile image)
         {
-            // eliminare l'immagine vecchia
-            //if (System.IO.File.Exists(fullPath))
-            //{
-            //    System.IO.File.Delete(fullPath);
-            //}
+            var error = true;
+            var conn = new SqlConnection(connString);
+            try
+            {
+                // validare i dati
 
-            // salviamo il file che ci è stato inviato
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
-            string fileName = Path.GetFileName(image.FileName);
-            string fullFilePath = Path.Combine(path, fileName);
-            //string fullFilePath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\uploads\\{Path.GetFileName(image.FileName)}";
-            FileStream stream = new FileStream(fullFilePath, FileMode.Create);
-            image.CopyTo(stream);
+                conn.Open();
 
+                // salviamo il file che ci è stato inviato
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+                string fileName = Path.GetFileName(image.FileName);
+                string fullFilePath = Path.Combine(path, fileName);
+                // TODO: generare un nome univoco
+                //string fullFilePath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\uploads\\{Path.GetFileName(image.FileName)}";
+                FileStream stream = new FileStream(fullFilePath, FileMode.Create);
+                image.CopyTo(stream);
 
-            // validare i dati
-            var newFish = StaticDb.Add(fish.Name, fish.IsSeaFish, fish.Price * 100);
-            //return Redirect("https://localhost:7029/fish/details/" + fish.FishId);
-            return RedirectToAction("Details", new { id = newFish.FishId });
+                // creare il comando
+                var command = new SqlCommand(@"
+                    INSERT INTO Fish
+                    (Name, IsSeaFish, Price, Image) VALUES
+                    (@name, @isSeaFish, @price, @image)", conn);
+                command.Parameters.AddWithValue("@name", fish.Name);
+                command.Parameters.AddWithValue("@isSeaFish", fish.IsSeaFish);
+                command.Parameters.AddWithValue("@price", fish.Price);
+                // questo valore lo ricaviamo dopo aver salvato l'immagine nel disco
+                command.Parameters.AddWithValue("@image", fileName);
+
+                // eseguire il comando
+                var nRows = command.ExecuteNonQuery();
+                error = false;
+            }
+            catch (Exception ex) {}
+            finally
+            {
+                conn.Close();
+            }
+
+            if (error) return View("Error"); else return RedirectToAction("Index");
+            // l'ExecuteNonQuery non ritorna la risorsa creata ma solo il numero di righe
+            //return RedirectToAction("Details", new { id = newFish.FishId });
         }
 
         // pagina con un form per la modififica di un pesce
@@ -157,18 +178,75 @@ namespace Pescheria.Controllers
         [HttpGet]
         public IActionResult Delete(int? id)
         {
-            var fish = StaticDb.GetById(id);
-            return View(fish);
+            if (id is not null)
+            {
+                var fish = getById((int)id);
+                return View(fish);
+            }
+            return RedirectToAction("Index");
+            
         }
 
         [HttpPost]
         public IActionResult DeletePost(Fish fish)
         {
-            var fishDeleted = StaticDb.HardDelete(fish.FishId);
-            // qui so che la risorsa è stata eliminata e quale
-            if (fishDeleted is not null)
+            var error = true;
+            var deletedFish = new Fish();
+            var conn = new SqlConnection(connString);
+            try
             {
-                TempData["MessageSuccess"] = $"Il pesce {fishDeleted.Name} è stato eliminato";
+                // oppure fare con i parametri di output
+                conn.Open();
+                // creare il comando
+                var commandRead = new SqlCommand("select * from Fish where FishId=@fishId", conn);
+                commandRead.Parameters.AddWithValue("@fishId", fish.FishId);
+
+                // eseguire il comando
+                var reader = commandRead.ExecuteReader();
+
+                // usare i dati
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    deletedFish.FishId = (int)reader["FishId"];
+                    deletedFish.Name = reader["Name"].ToString();
+                    deletedFish.IsSeaFish = (bool)reader["IsSeaFish"];
+                    deletedFish.Price = (int)reader["Price"];
+                    // deletedFish.DeletedAt = (DateTime)reader["DeletedAt"]; // TODO: check
+                    deletedFish.Image = reader["Image"].ToString();
+                }
+
+                reader.Close();
+
+                // creare il comando
+                var commandDelete = new SqlCommand("DELETE FROM Fish WHERE  FishId=@fishId", conn);
+                commandDelete.Parameters.AddWithValue("@fishId", fish.FishId);
+
+                // eseguire il comando
+                var nRows = commandDelete.ExecuteNonQuery();
+                if (nRows > 0) {
+                    error = false;
+
+                    // eliminare anche l'immagine
+                    if (deletedFish.Image is not null)
+                    {
+                        string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", deletedFish.Image);
+                        if (System.IO.File.Exists(fullPath))
+                        {
+                            System.IO.File.Delete(fullPath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                conn.Close();
+            }
+
+            if (!error)
+            {
+                TempData["MessageSuccess"] = $"Il pesce {deletedFish.Name} è stato eliminato";
                 return RedirectToAction("Index");
             }
 
@@ -213,6 +291,42 @@ namespace Pescheria.Controllers
         public IActionResult GetSlug()
         {
             return Json("questo-slug-buono");
+        }
+
+        private Fish? getById(int id)
+        {
+            var error = true;
+            var fish = new Fish();
+
+            // aprire la connessione
+            var conn = new SqlConnection(connString);
+            try
+            {
+                conn.Open();
+                // creare il comando
+                var command = new SqlCommand("select * from Fish where FishId=@fishId", conn);
+                command.Parameters.AddWithValue("@fishId", id);
+
+                // eseguire il comando
+                var reader = command.ExecuteReader();
+
+                // usare i dati
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    fish.FishId = (int)reader["FishId"];
+                    fish.Name = reader["Name"].ToString();
+                    fish.IsSeaFish = (bool)reader["IsSeaFish"];
+                    fish.Price = (int)reader["Price"];
+                    // fish.DeletedAt = (DateTime)reader["DeletedAt"]; // TODO: check
+                    fish.Image = reader["Image"].ToString();
+                    error = false;
+                }
+            }
+            catch (Exception ex) { }
+            finally { conn.Close(); }
+
+            if (error) return null; else return fish;
         }
     }
 }
